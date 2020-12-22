bc364b098da3a66412ace7d5805d710a39834ead
dbhesaniya <dipakbhesaniya1009@gmail.com>
2020-12-22 22:01:16 +0530

    End of section 4 - services and parent-chile input/output

---
+++ DatingApp-SPA/src/app/nav/nav.component.ts
@@ -0,0 +1,36 @@
+import { Component, OnInit } from '@angular/core';
+import { AuthService } from '../_services/auth.service';
+
+@Component({
+  selector: 'app-nav',
+  templateUrl: './nav.component.html',
+  styleUrls: ['./nav.component.scss'],
+})
+export class NavComponent implements OnInit {
+  model: any = {};
+
+  constructor(private authService: AuthService) {}
+
+  ngOnInit() {}
+
+  login() {
+    this.authService.login(this.model).subscribe(
+      (next) => {
+        alert('Logged in successfully');
+      },
+      (error) => {
+        alert('Failed to login');
+      }
+    );
+  }
+
+  loggedIn() {
+    const token = localStorage.getItem('token');
+    return !!token;
+  }
+
+  logout() {
+    localStorage.removeItem('token');
+    alert('logged out');
+  }
+}
